ad7b4fb02b84eda559d4555d5a3daf2534797114
Jacob Moeller <jacobmoe@gmail.com>
2014-07-15 22:24:10 -0400

    dump returned json into view for now

--- app/scripts/controller/home.js
+++ app/scripts/controller/home.js
@@ -2,7 +2,7 @@ angular.module('wikiwash').controller('HomeController', ['$scope', 'mediawikiApi
   function($scope, mediawikiApi) {
   
     $scope.pageName = "";
-    $scope.pageData = "Data will go here";
+    $scope.pageData = "";
 
     $scope.searchWiki = function() {
       mediawikiApi.getPage($scope.pageName, function (data) {

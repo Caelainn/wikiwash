b18f90b26240a5b55afafbfe0f43232315acf7f6
arovillard <arovillard@gmail.com>
2014-07-17 10:51:35 -0400

    basic split

--- config/routes.js
+++ config/routes.js
@@ -9,15 +9,19 @@ module.exports = function(app, io) {
   app.get('/api/pages/:id', function(req, res) {
     users.index(req, res)
   });
-  
+
   app.all('/', function (req, res) {
     res.sendfile('landing.html', { root: root })
   })
-  
+
+  app.all('/temp', function (req, res) {
+    res.sendfile('index_temp.html', { root: root })
+  })
+
   app.all('/:page', function (req, res) {
     events(io, req.params.id);
     res.sendfile('index.html', { root: root })
   })
-  
+
 }
 

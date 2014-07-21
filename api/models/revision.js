32b0a76e704501cfde49c4f695e6e714724d1c59
arovillard <arovillard@gmail.com>
2014-07-21 18:57:14 -0400

    add footer to left content

--- api/models/revision.js
+++ api/models/revision.js
@@ -6,10 +6,10 @@ var fs = require('fs');
 var endPoint = 'en.wikipedia.org';
 
 var queryPath = function (revisionId, previousRevisionId) {
-  return "/w/api.php?" + 
-         "action=query&" + 
-         "prop=revisions&" + 
-         "format=json&" + 
+  return "/w/api.php?" +
+         "action=query&" +
+         "prop=revisions&" +
+         "format=json&" +
          "rvprop=ids%7Cuser%7Cuserid%7Ccomment%7Ccontent" +
          "&rvparse&" +
          "revids=" + revisionId + "|" + previousRevisionId
@@ -18,7 +18,7 @@ var queryPath = function (revisionId, previousRevisionId) {
 var revisionDiffHtml = function (json, revisionId, previousRevisionId) {
   var queryResPages = json['query']['pages'];
   var queryResPage = queryResPages[Object.keys(queryResPages)[0]];
-  
+
   var revisionHtml = _.find(queryResPage.revisions, function (r) {
     return r.revid == revisionId;
   })['*'];
@@ -26,18 +26,18 @@ var revisionDiffHtml = function (json, revisionId, previousRevisionId) {
   var previousHtml = _.find(queryResPage.revisions, function (r) {
     return r.revid == previousRevisionId;
   })['*'];
-  
+
   var result = "";
-  
+
   try {
     var diff = new gdiff();
 
     var diffParts = diff.diff_main(previousHtml, revisionHtml);
     diffParts.forEach(function (part) {
       if (part[0] > 0) {
-        result = result + '<span style="color: green;">' + part[1] + '</span>';
+        result = result + '<span style="background-color: #B8E9D2 !important;">' + part[1] + '</span>';
       } else if (part[0] < 0) {
-        result = result + '<span style="color: red;">' + part[1] + '</span>';
+        result = result + '<span style="background-color: #F1BCBC !important;">' + part[1] + '</span>';
       } else {
         result += part[1];
       }
@@ -45,7 +45,7 @@ var revisionDiffHtml = function (json, revisionId, previousRevisionId) {
   } catch (err) {
     result = 'Diff unavailable';
   };
-  
+
   return result;
 };
 
@@ -55,9 +55,9 @@ module.exports.getRevisionDiff = function (revisionId, previousRevisionId, callb
     host: endPoint,
     path: queryPath(revisionId, previousRevisionId)
   };
-  
+
   console.log(options);
-  
+
   http.request(options).then(function (response) {
     response.body.read().then(function (body) {
       var json = JSON.parse(body);

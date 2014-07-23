1c33d1c795307d455b2dcd7ef1c32b4e8f641be1
Jacob Moeller <jacobmoe@gmail.com>
2014-07-22 21:19:04 -0400

    remove api injection from home controller

--- app/scripts/controller/home.js
+++ app/scripts/controller/home.js
@@ -1,5 +1,5 @@
 angular.module('wikiwash').controller('HomeController', ['$scope', '$location', 'socketService',
-  function($scope, $location, mediawikiApi, socketService) {
+  function($scope, $location, socketService) {
     $scope.pageName = '';
     $scope.revisions = [];
 

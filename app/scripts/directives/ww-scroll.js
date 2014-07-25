7832a289806161090ed203a8e5b1c74205808cac
Jacob Moeller <jacobmoe@gmail.com>
2014-07-25 12:12:58 -0400

    highlight current eidt

--- app/scripts/directives/ww-scroll.js
+++ app/scripts/directives/ww-scroll.js
@@ -2,13 +2,26 @@ angular.module('wikiwash')
   .directive('wwScroll', ['SmoothScroll', function(SmoothScroll) {
     return {
       link: function(scope, element, attr) {
+        var prevEdit = null;
 
         scope.$watch('nextEdit', function (current, last) {
           var edit = $('#edit-' + last);
+          
+          if (prevEdit) {
+            if (prevEdit.hasClass('additions')) {
+              prevEdit.css('background-color', '#B8E9D2');
+            } else if (prevEdit.hasClass('subtractions')) {
+              prevEdit.css('background-color', '#F1BCBC');
+            }
+          };
 
           if (edit.length > 0) {
-            $('.ww-edit').css('text-decoration', 'none');
-            edit.css('text-decoration', 'underline');
+            if (edit.hasClass('additions')) {
+              edit.css('background-color', '#55f2a3');
+            } else if (edit.hasClass('subtractions')) {
+              edit.css('background-color', '#f99593');
+            }
+
             setTimeout(function () {
               element.animate({
                 scrollTop: edit.offset().top - element.offset().top + element.scrollTop()
@@ -16,6 +29,7 @@ angular.module('wikiwash')
             }, 500);
           }
           
+          prevEdit = edit;
           console.log("ww-sroll");
         });
       }

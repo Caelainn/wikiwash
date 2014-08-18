fcde4419e39a6a9833439849c9a292be7c2324c3
Jacob Moeller <jacobmoe@gmail.com>
2014-08-18 11:54:01 -0400

    allow http urls in search

--- app/scripts/services/page-parser.js
+++ app/scripts/services/page-parser.js
@@ -2,7 +2,7 @@ angular.module('wikiwash').factory('pageParser', [function() {
   return {
     getPageName: function (url) {
       return url.replace("en.wikipedia.org/wiki/", "")
-                .replace("https://", "")
+                .replace(/http(s|):\/\//, "")
                 .replace(/ /g, "_");
     }
   };

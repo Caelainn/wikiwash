8ec530a92f5e3c17ab6b298d697768526b875cc0
Jacob Moeller <jacobmoe@gmail.com>
2014-09-23 22:10:21 -0400

    cleanup directives

---
+++ app/scripts/directives/fixed-height.js
@@ -0,0 +1,10 @@
+angular.module('wikiwash').directive('fixedHeight', function ($window) {
+  return {
+    scope: {
+      offset: "@"
+    },
+    link: function (scope, element) {
+      element.height($window.innerHeight - scope.offset);
+    }
+  }
+})

8d93ad266920c3dc91d16e5b669814e83650fcc9
Jacob Moeller <jacobmoe@gmail.com>
2014-10-10 15:09:42 -0400

    add social directive

---
+++ app/scripts/directives/social-buttons.js
@@ -0,0 +1,19 @@
+angular.module('wikiwash')
+.directive('socialButtons', function($window, $location) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/views/directives/social-buttons.html',
+    controller: function($scope, $location){
+      $scope.urls = {
+        facebook: "https://www.facebook.com/sharer/sharer.php?u=",
+        linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=",
+        twitter: "https://twitter.com/home?status=" 
+      };
+
+      $scope.shareUrl = function(url) {
+        return url + $location.absUrl();
+      };
+    }
+  };
+});

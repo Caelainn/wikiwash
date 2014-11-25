6800c8fd68c6bb908e343b561676eb1aabaf71f4
Peter Sobot <psobot@gmail.com>
2014-11-25 15:22:34 -0500

    Fixed precaching script to use local data files when possible.

--- scripts/PreCacheTopArticles.js
+++ scripts/PreCacheTopArticles.js
@@ -1,13 +1,15 @@
-var moment = require('moment'),
-    Q      = require('q'),
-    _      = require('lodash');
+var Q    = require('q'),
+    fs   = require('fs'),
+    path = require('path'),
+    _    = require('lodash');
 
 var log         = require('../backend/config/log').createLoggerForFile(__filename);
 var TopArticles = require('../backend/helpers/TopArticles.js');
 var Page        = require('../backend/models/Page');
 
-log.info("Fetching top articles from the past hour...");
-TopArticles().then(function(data) {
+var targetPath = path.join(__dirname, '..', 'data', TopArticles.topArticlesFilename);
+
+var processArticleEntries = function(data) {
   log.info("Fetched " + data.length + " top articles from the past hour.");
   var funcs = _.map(data, function(element) {
     var page = element[0];
@@ -22,4 +24,23 @@ TopArticles().then(function(data) {
     };
   });
   return funcs.reduce(Q.when, Q());
-}).done();
+};
+
+try {
+  log.info("Trying to grab top articles from local cache file...");
+  var stat = fs.statSync(targetPath);
+  
+  var now = +new Date();
+  var fileUpdatedAt = +stat.mtime;
+
+  if (fileUpdatedAt - now > (60 * 60 * 1000)) {
+    log.info("Top articles list is out of date. Re-fetching...");
+    TopArticles().then(processArticleEntries).done();
+  } else {
+    var file = fs.readFileSync(targetPath, {encoding: 'utf-8'});
+    processArticleEntries(JSON.parse(file)).done();
+  }
+} catch (e) {
+  log.info("Fetching top articles from the past hour...");
+  TopArticles().then(processArticleEntries).done();
+}

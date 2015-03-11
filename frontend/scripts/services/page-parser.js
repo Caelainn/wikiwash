e8af40c20e87a2b224b8c79cd3328abf6c914399
Scott Tadman <scott@twg.ca>
2015-03-11 17:37:08 -0400

    Relaxing regular expression for procesing home page searches, fixes #84

--- frontend/scripts/services/page-parser.js
+++ frontend/scripts/services/page-parser.js
@@ -1,7 +1,7 @@
 angular.module('wikiwash').factory('pageParser', [function() {
   return {
     getPageName: function (url) {
-      return url.replace("en.wikipedia.org/wiki/", "")
+      return url.replace(/(en\.)?wikipedia.org\/wiki\//, "")
                 .replace(/http(s|):\/\//, "")
                 .replace(/ /g, "_");
     }

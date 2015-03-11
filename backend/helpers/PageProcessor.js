5d76418b0cff8ee78a1a16469fa188ccf24c2a24
Scott Tadman <scott@twg.ca>
2015-03-11 19:35:54 -0400

    Force image links to go to Wikipedia, fixes #72

--- backend/helpers/PageProcessor.js
+++ backend/helpers/PageProcessor.js
@@ -3,15 +3,23 @@ var cheerio = require('cheerio');
 function process(html) {
   var doc = cheerio.load(html);
 
-  doc('a').each(function(i, e) {
-    e = doc(e);
-    // /w/index.php?title=George_Clooney&redirect=no
-
+  doc('a').each(function(i, _e) {
+    var e = doc(_e);
     var href = e.attr('href');
 
-    href = href.replace(/^\/w\/index.php\?title=/, '/wiki/').replace(/\&.*/, '')
+    if (href.match(/File:/)) {
+      // Image pages cannot be processed within the WikiWash application
+      href = 'http://en.wikipedia.org' + href;
 
-    doc(e).attr('href', href);
+      // Open in a new window
+      e.attr('target', '_blank');
+    }
+    else {
+      // Handle legacy Wikipedia URLs.
+      href = href.replace(/^\/w\/index.php\?title=/, '/wiki/').replace(/\&.*/, '')
+    }
+
+    e.attr('href', href);
   })
 
   return doc.html();

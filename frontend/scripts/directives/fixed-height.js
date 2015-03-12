3c085443b57fa4edaa701fe9612ed2b0a5918065
Scott Tadman <scott@twg.ca>
2015-03-12 19:59:49 -0400

    Fixing fixed-height, making it respond to page resize events, fixes #87, fixes #43

--- frontend/scripts/directives/fixed-height.js
+++ frontend/scripts/directives/fixed-height.js
@@ -1,13 +1,20 @@
-angular.module('wikiwash').directive(
-  'fixedHeight',
-  function ($window) {
-    return {
-      scope: {
-        offset: "@"
-      },
-      link: function (scope, element) {
-        element.height($window.innerHeight - scope.offset);
-      }
-    };
-  }
-);
+angular.module('wikiwash')
+  .directive(
+    'fixedHeight',
+    function($window) {
+      return {
+        scope: {
+          offset: "@"
+        },
+        link: function (scope, element) {
+          var resizeHandler = function() {
+            element.height($window.innerHeight - scope.offset - 25);
+          };
+
+          angular.element($window).bind('resize', resizeHandler);
+
+          resizeHandler();
+        }
+      };
+    }
+  );

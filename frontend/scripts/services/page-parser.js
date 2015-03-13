32c3bc42783dea3e0b7a210737a6d12c3a81d96d
Scott Tadman <scott@twg.ca>
2015-03-13 16:18:43 -0400

    Avoid over-encoding, fixes #75

--- frontend/scripts/services/page-parser.js
+++ frontend/scripts/services/page-parser.js
@@ -1,11 +1,9 @@
 angular.module('wikiwash').factory('pageParser', [function() {
   return {
     getPageName: function (url) {
-      return encodeURIComponent(
-        url.replace(/(en\.)?wikipedia.org\/wiki\//, "")
+      return url.replace(/(en\.)?wikipedia.org\/wiki\//, "")
           .replace(/https?:\/\//, "")
-          .replace(/ /g, "_")
-      );
+          .replace(/ /g, "_");
     }
   };
 }]);
